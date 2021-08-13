fb85ac484fd51857230c84da575df908b0ff863b
sudojanuc <janpaschka@icloud.com>
2021-08-13 19:50:59 +0200

    changed redirect uri

--- fitness-challenge/src/environments/environment.prod.ts
+++ fitness-challenge/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   CONCEPT2_API : 'https://log.concept2.com', 
   client_id: 'asWj9Gh7mrXWZI0JjoyLHP2aP2ytQV4dYQrX4w0k',
   client_secret: '',
-  redirect_uri: 'https://fitness-challenge.on.fleek.co'
+  redirect_uri: 'https://fitness-challenge.on.fleek.co/'
 };
